da42605f3c48dbf784c8416029fde4c268ff302b
Justin Polis <j.polikevicius@gmail.com>
2024-01-27 12:36:07 +0200

    config

--- module.nix
+++ module.nix
@@ -8,8 +8,8 @@ let
 in {
   # Declare what settings a user of this "hello.nix" module CAN SET.
   options.services.hello = {
-    enable = mkEnableOption "hello service";
-    greeter = mkOption {
+    enable = lib.mkEnableOption "hello service";
+    greeter = lib.mkOption {
       type = types.str;
       default = "world";
     };
@@ -18,7 +18,7 @@ in {
   # Define what other settings, services and resources should be active IF
   # a user of this "hello.nix" module ENABLED this module 
   # by setting "services.hello.enable = true;".
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     systemd.services.hello = {
       wantedBy = [ "multi-user.target" ];
       serviceConfig.ExecStart =
