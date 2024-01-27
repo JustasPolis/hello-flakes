d5b74af668df8de0c114c20e856bd45fce42b9a2
Justin Polis <j.polikevicius@gmail.com>
2024-01-27 11:05:37 +0200

    config

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      inherit (self) outputs;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = (import ./hello.nix { inherit pkgs; });
+      nixosModules.default = (import ./module.nix { inherit inputs; });
+    };
+}
