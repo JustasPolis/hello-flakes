638a427ed20100335b639720fed249c21d6e9256
Justin Polis <j.polikevicius@gmail.com>
2024-01-27 19:51:32 +0200

    config

--- flake.nix
+++ flake.nix
@@ -6,9 +6,9 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       lib = nixpkgs.lib;
-      config = pkgs.config;
+      config = nixpkgs.config;
     in {
       packages.${system}.default = (import ./default.nix { inherit pkgs; });
-      nixosModules.hello = (import ./module.nix { inherit lib config pkgs; });
+      nixosModules.hello = (import ./module.nix { inherit config lib pkgs; });
     };
 }
