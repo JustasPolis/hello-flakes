7c770407eebcf2414c4720a24eff547fd95ade8d
Justin Polis <j.polikevicius@gmail.com>
2024-01-27 19:54:15 +0200

    config

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      lib = nixpkgs.lib;
-      config = nixpkgs.config;
     in {
       packages.${system}.default = (import ./default.nix { inherit pkgs; });
-      nixosModules.hello = (import ./module.nix { inherit config lib pkgs; });
+      nixosModules.hello = (import ./module.nix { inherit inputs pkgs; });
     };
 }
